e6cb7bb2018e7ba76ae0cc0e658b4468b3104dd1
Thor-ux <alongeayomidedaniel@gmail.com>
2025-11-14 06:22:06 -0800

    phones

--- databases/work_with_database/phones/management/commands/import_phones.py
+++ databases/work_with_database/phones/management/commands/import_phones.py
@@ -1,20 +1,29 @@
 import csv
-
 from django.core.management.base import BaseCommand
-from phones.models import Phone
-
+from your_app.models import Phone
+from django.utils.text import slugify
+from datetime import datetime
 
 class Command(BaseCommand):
-    def add_arguments(self, parser):
-        pass
+    help = "Import phones from CSV"
 
     def handle(self, *args, **options):
-        with open('phones.csv', 'r') as csvfile:
-
-            phone_reader = csv.reader(csvfile, delimiter=';')
-            # пропускаем заголовок
-            next(phone_reader)
-
-            for line in phone_reader:
-                # TODO: Добавьте сохранение модели
-                pass
+        file_path = 'phones.csv' 
+        with open(file_path, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=';') 
+            for row in reader:
+                phone, created = Phone.objects.get_or_create(
+                    id=row['id'],
+                    defaults={
+                        'name': row['name'],
+                        'price': float(row['price']),
+                        'image': row['image'],
+                        'release_date': datetime.strptime(row['release_date'], '%Y-%m-%d').date(),
+                        'lte_exists': row['lte_exists'].lower() in ['true', '1', 'yes'],
+                        'slug': slugify(row['name']),
+                    }
+                )
+                if created:
+                    self.stdout.write(f"Create {phone.name}")
+                else:
+                    self.stdout.write(f"Skip {phone.name}")
--- databases/work_with_database/phones/views.py
+++ databases/work_with_database/phones/views.py
@@ -1,13 +1,28 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, get_object_or_404
+from .models import Phone
 
 def show_catalog(request):
-    template = 'catalog.html'
-    context = {}
-    return render(request, template, context)
+    sort = request.GET.get('sort', '')  # sorting parameter
+
+    phones = Phone.objects.all()
+
+    if sort == 'name':
+        phones = phones.order_by('name')
+    elif sort == 'min_price':
+        phones = phones.order_by('price')
+    elif sort == 'max_price':
+        phones = phones.order_by('-price')
+
+    context = {
+        'phones': phones,
+        'current_sort': sort,
+    }
+    return render(request, 'catalog.html', context)
 
 
 def show_product(request, slug):
-    template = 'product.html'
-    context = {}
-    return render(request, template, context)
+    phone = get_object_or_404(Phone, slug=slug)
+    context = {
+        'phone': phone,
+    }
+    return render(request, 'product.html', context)
